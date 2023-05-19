4cf83158d99e74c0184e9a8de7e1d09c4e6cbe0b
Murilo Teixeira <murilogteixeira@gmail.com>
2023-05-18 22:14:35 -0300

    Criar projeto inicial

---
+++ ProjetoIrmandade/ProjetoIrmandade/ProjetoIrmandadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoIrmandadeApp.swift
+//  ProjetoIrmandade
+//
+//  Created by Murilo Teixeira on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoIrmandadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
